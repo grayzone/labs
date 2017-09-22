700107a65fd087f0afd11e4f37dc07b386a9c265
grayzone <grayzone137@gmail.com>
2017-09-22 19:23:39 +0800

    add store folder.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./containers/Counter";
-import {createStore} from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers";
-//import store from "./store";
+import store from "./store";
 
 var ContainersDIV = document.getElementById("containers");
-if (ContainersDIV != null) {
-  const store = createStore(reducer);
+if (ContainersDIV != null) {  
   ReactDOM.render(
     <Provider store={store}>
       <Counter />
